5f06a53046bcfdd2f857c808ca62a3f3b5f1f6f6
Rasha Moumneh <rasha@fastmail.com>
2020-03-23 13:54:42 -0400

    get max

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -145,4 +145,14 @@ class DoublyLinkedList:
     """Returns the highest value currently in the list"""
 
     def get_max(self):
-        pass
+        # loop through all nodes looking for biggest value
+        # TODO: error checking
+        if not self.head:
+            return None
+        max_value = self.head.value
+        current = self.head
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
